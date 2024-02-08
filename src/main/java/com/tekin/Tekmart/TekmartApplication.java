1cd22b5f6b774e3188f544ad33c99fdca27f296b
vakavijay083 <vijaybhaskarreddy@email.saintleo.edu>
2024-02-08 17:10:01 -0500

    First Commit

---
+++ src/main/java/com/tekin/Tekmart/TekmartApplication.java
@@ -0,0 +1,13 @@
+package com.tekin.Tekmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TekmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TekmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tekin/Tekmart/TekmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tekin.Tekmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TekmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
